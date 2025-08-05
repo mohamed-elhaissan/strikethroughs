1e7fd7094bf51acd0fa2934303dfca060acd9f43
mohamed-elhaissan <mhalwasoffice@gmail.com>
2025-08-05 21:05:40 +0100

    :(

---
+++ app/components/PlayFul.tsx
@@ -0,0 +1,8 @@
+export default function PlayFul() {
+  return (
+    <div className="dark:bg-white/[.06] p-2 px-4 text-base rounded-xl">
+      <input type="checkbox" />
+      Code in Assembly 💾
+    </div>
+  );
+}
